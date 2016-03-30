bf8c65b34b43c80dd3a899bf17e6c72c4eea952b
andelar96 <andelar96@gmail.com>
2016-03-30 19:40:15 +0200

    Added temporary options button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-    // Enter key binding in the input field
-    $('#input').keypress(function (key) {
-        if (key.which === 13) {
-            console.log("ENTER"); // placeholder
-        }
-    });
-});
+  // Enter key binding in the input field
+  $('#input').keypress(function (key) {
+    if (key.which === 13) {
+      console.log('ENTER') // placeholder
+    }
+  })
+
+  // Temperorary
+  // TODO: Move functionality to react.jsx
+  $('#options_button').on('click', function (e) {
+    $('#options').fadeToggle(200)
+    $('#input').focus()
+  })
+})
