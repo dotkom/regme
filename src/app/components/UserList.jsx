beba0a63c1b2ddcbee77bd2478a5e8f1c348f4b7
niklasmh <nikasmh@hotmail.com>
2016-04-13 23:45:39 +0200

    Imported React and react-redux to files, as it's needed to render and connect to state

--- src/app/components/UserList.jsx
+++ src/app/components/UserList.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { connect } from 'react-redux'
 
 class UserList extends React.Component {
   getInitialState () {
--- src/app/components/UserLists.jsx
+++ src/app/components/UserLists.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { connect } from 'react-redux'
 
 class UserLists extends React.Component {
   getInitialState () {
