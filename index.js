25848906f251f6bce2553259bae27ebb468f51e5
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-24 22:17:28 +0200

    Added Registration component to App

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header'
+import Registration from './components/registration'
 
 const App = () => {
   return(
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <Header />
-      <main className="mdl-layout__content">
-        <div className="page-content">Here there be content</div>
-      </main>
+      <Registration />
     </div>
   )
 }
