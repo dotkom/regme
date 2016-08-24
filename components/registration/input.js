4e6f03b4b3a646fe1fb003b31f702947fab326f4
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-25 00:28:28 +0200

    Added JSDoc comment to the Input component

--- components/registration/input.js
+++ components/registration/input.js
@@ -1,3 +1,8 @@
+/**
+ * The input field. Should only fetch input from
+ * user or RFID-reader, then let the core
+ * application validate the rest.
+ */
 const Input = ( { placeholder } ) => {
   return (
     <input placeholder={ placeholder } />
