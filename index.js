52846fe91e6498ffa4ad44041931778ba0e43815
Sardred <andelar96@gmail.com>
2016-08-24 19:39:51 +0200

    Added first component

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('kek')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header'
+
+const App = () => {
+  return(
+    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <Header />
+      <main className="mdl-layout__content">
+        <div className="page-content">Here there be content</div>
+      </main>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
