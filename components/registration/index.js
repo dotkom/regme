f9957e985a5b334a59dbd3a297e044936ca995df
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-24 22:16:49 +0200

    Added registration component with input

---
+++ components/registration/index.js
@@ -0,0 +1,12 @@
+import Input from './input'
+
+const Registration = () => {
+  return (
+    <div>
+      <div>Registrering!</div>
+      <Input placeholder="Skriv inn her....." />
+    </div>
+  )
+}
+
+export default Registration
---
+++ components/registration/input.js
@@ -0,0 +1,7 @@
+const Input = ( { placeholder } ) => {
+  return (
+    <input placeholder={ placeholder } />
+  )
+}
+
+export default Input
