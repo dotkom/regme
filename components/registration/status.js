247bb8c2108e3f45505335833fcf7ca11cc02619
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-25 00:36:35 +0200

    Added double digit format to timer on status

--- components/registration/status.js
+++ components/registration/status.js
@@ -9,10 +9,20 @@
  * @prop {object} time Time object with hour, minute and second
  */
 const Status = ( { message, time } ) => {
+  /**
+   * Double digit short function. Making the input
+   * number to a string with 2 digits minimum.
+   * 
+   * @param {number} number
+   */
+  let dd = (number) => {
+    return (number < 10 ? '0' : '') + number
+  }
+
   return (
     <div className="color">
       <div className="left">{ message }</div>
-      <div className="right">{ `${time.hour}:${time.minute}:${time.second}` }</div>
+      <div className="right">{ `${dd(time.hour)}:${dd(time.minute)}:${dd(time.second)}` }</div>
     </div>
   )
 }
