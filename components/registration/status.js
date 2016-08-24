6c05e9c42c7260588d2383af7776d07b9b40ba50
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-25 00:25:27 +0200

    Added JSDoc comment to the Status component

--- components/registration/status.js
+++ components/registration/status.js
@@ -1,3 +1,13 @@
+/**
+ * Showing the status of the application.
+ * Component should:
+ *  - Display color appropriate for situation.
+ *  - Show message for situation.
+ *  - Show time when situation was created.
+ * 
+ * @prop {string} message The current feedback
+ * @prop {object} time Time object with hour, minute and second
+ */
 const Status = ( { message, time } ) => {
   return (
     <div className="color">
