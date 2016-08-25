17db4a429019f7df3be7c649bac3de22c323204f
Sardred <andelar96@gmail.com>
2016-08-25 16:20:53 +0200

    Fixed overlooked merge stuff

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header'
-<<<<<<< 49cc0e03de8937597b85afefb382e37ab4b9997e
 import Registration from './components/registration'
-=======
 import Options from './components/options'
->>>>>>> Added basic options
 
 const App = () => {
   let title = "Bedrift"
