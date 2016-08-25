c2f50213597f0ea067ab92bfc295d29b62c19a57
Sardred <andelar96@gmail.com>
2016-08-25 21:26:05 +0200

    Added functioning option button

--- components/options/index.js
+++ components/options/index.js
@@ -1,15 +1,33 @@
 import Events from './events'
 import Attendees from './attendees'
 
-const Options = () => {
-  return (
-    <div>
-      <Events />
-      <hr />
-      <Attendees />
-    </div>
-  );
-};
+
+const Options = React.createClass({
+  getInitialState: function (){
+    return{ showOptions: false }
+  },
+  clickHandler: function () {
+    this.setState({ showOptions: !this.state.showOptions })
+  },
+  render: function () {
+    let optionBody = ''
+    if(this.state.showOptions){
+      optionBody = (
+        <div>
+          <Events />
+          <hr />
+          <Attendees />
+        </div>
+      )
+    }
+    return(
+      <div>
+        <input type='button' value='Alternativer' onClick={ this.clickHandler } />
+        { optionBody }
+      </div>
+    )
+  }
+})
 
 export default Options;
 
