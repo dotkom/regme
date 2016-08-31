d02ec9529179460ee7d2d1c0dc3f32a4e9071fb7
Sardred <andelar96@gmail.com>
2016-08-31 19:58:16 +0200

    Add logo to header

--- components/header/index.js
+++ components/header/index.js
@@ -2,7 +2,9 @@ const Header = () => {
   return (
     <header className='mdl-layout__header'>
       <div className='mdl-layout__header-row'>
-        <span className='mdl-layout__title'>Online</span>
+        <span className='mdl-layout__title'>
+          <img className="about-header-logo" src="assets/images/online_logo.svg" alt="Online" />
+        </span>
       </div>
     </header>
   );
