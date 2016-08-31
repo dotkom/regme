dcecb830d642a79dc2aecf8148dca2ad39f3a0e5
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-08-31 23:21:19 +0200

    Added classnames and made input in mdl style

--- components/registration/input.js
+++ components/registration/input.js
@@ -2,10 +2,20 @@
  * The input field. Should only fetch input from
  * user or RFID-reader, then let the core
  * application validate the rest.
+ * 
+ * @prop {string} placeholder The placeholder text for the input field.
  */
 const Input = ( { placeholder } ) => {
   return (
-    <input placeholder={ placeholder } />
+    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label registration-input">
+      <input className="mdl-textfield__input"
+        type="text"
+        id="registration-input"
+        pattern="[A-Za-z\s]+|[0-9]+" />
+      <label className="mdl-textfield__label"
+        htmlFor="registration-input">{ placeholder }</label>
+      <span className="mdl-textfield__error">Input er ikke gyldig!</span>
+    </div>
   )
 }
 
