aa00205a9925bd4e915dd07385325d21f6c271f2
Sardred <andelar96@gmail.com>
2016-09-02 11:48:25 +0200

    Added class to table to clarify less

--- components/options/attendees.js
+++ components/options/attendees.js
@@ -4,7 +4,7 @@ const Attendees = () => {
   return (
     <div>
       <h3>Deltakere</h3>
-      <table className='mdl-data-table mdl-js-data-table'>
+      <table className='mdl-data-table mdl-js-data-table attendee-lists'>
         <List category='Møtt' />
         <List category='Ikke møtt' />
         <List category='Venteliste' />
