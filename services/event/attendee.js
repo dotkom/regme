c759ef0324e1da2a0c3c54daa82e193699c542ad
Jarle Trollebø <aborysa@gmail.com>
2016-10-19 22:20:14 +0200

    'Refactor'

--- services/event/attendee.js
+++ services/event/attendee.js
@@ -1,10 +1,12 @@
 export class Attendee{
-  constructor(id,username,fullname,primary=false,reg_date?){
+  constructor(id,username,firstname,lastname,primary=false,registered=false){
     this._id = id
     this._username = username
-    this._fullname = fullname
-    this._reg_date = reg_date
+    this._firstname = firstname
+    this._lastname = lastname
+    this._registered = registered
     this._primary = primary
+    console.log(this.isPrimary(),this.isRegistered())
   }
   get id(){
     return this._id
@@ -12,11 +14,17 @@ export class Attendee{
   get username(){
     return this._username
   }
+  get firstname(){
+    return this._firstname
+  }
+  get lastname(){
+    return this._lastname
+  }
   get fullname(){
-    return this._fullname
+    return `${this._firstname} ${this._lastname}`
   }
   isRegistered(){
-    return this._reg_date != null
+    return this._registered
   }
   isPrimary(){
     return this._primary
