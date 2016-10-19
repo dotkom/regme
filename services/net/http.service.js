76e5b33e6e60e16f91c93972b93df21ba243c2d5
Jarle Trollebø <aborysa@gmail.com>
2016-10-19 22:14:24 +0200

    Finished http.service

--- services/net/http.service.js
+++ services/net/http.service.js
@@ -1,8 +1,7 @@
-import { Observable, ReplaySubject, } from 'rxjs'
+import { Observable, Subject, } from 'rxjs'
 
 import { API_BASE, API_AUTH, CLIENT_SECRET, CLIENT_ID } from 'common/constants'
 
-console.log(API_BASE,API_AUTH)
 /*
 interface IHttpService{
   get(): Observable<any>;
@@ -20,51 +19,63 @@ class HttpService{
     this.requestQueue = []
     this.auth_token = ""
     this.waitingForToken = false
-    this.renewToken()
+    //this.renewToken()
   }
+  
   renewToken(){
     if(!this.waitingForToken){
-      this.post(API_BASE + API_AUTH,{
+      this.waitingForToken = true
+      this.post(`${API_BASE}${API_AUTH}`,{
         client_secret: CLIENT_SECRET,
         client_id: CLIENT_ID,
         grant_type: "client_credentials",
-      },true).subscribe((data) => {
-        this.auth_token = data.access_token;
-        //Performe requests from request queue
-        for(let i of this.requestQueue){
-          this.request(i)
-        }
-        this.requestQueue = {}
+      },true).delay(300).subscribe((data) => {
+          this.auth_token = data.access_token
+          //Performe requests from request queue
+          for(let i of this.requestQueue){
+            this.request(i.request).subscribe(r => {
+              i.subject.next(r)
+              i.subject.complete()
+            })
+          }
+          this.requestQueue = {}
       },(e)=>{
         console.log("Error",e)
       },() => {
         //Use a timeout to prevent a feedback loop
-        this.waitingForToken = false
-      });
+        setTimeout(()=>{
+          this.waitingForToken = false
+        },5000)
+      })
     }
   }
+
   handleResponse(r,req){
     if(!r.ok){
       if(r.status == 401){
         //Add request to queue
-        this.requestQueue.push(req)
-        //Renew if not waiting for token
+        let resolver = new Subject()
+        this.requestQueue.push({request: req, subject: resolver})
+        //Renew token if not waiting for token, because access denied
         this.renewToken()
+        return resolver.asObservable()
       }
       return Observable.throw(r)
     }
     return r.json()
   }
+
   request(request){
     //Add token to request
-    request.headers.set("Authorization", "Bearer " + this.auth_token)
+    request.headers.set("Authorization", `Bearer ${this.auth_token}`)
     return Observable.fromPromise(fetch(request))
       .flatMap(r => this.handleResponse(r,request))
   }
+  
   get(url,params){
     let pUrl = url
     if(params){
-      pUrl += HttpService.urlEncode(params);
+      pUrl += HttpService.urlEncode(params)
     }
     let request = new Request(pUrl,{method:'get'})
     return this.request(request)
