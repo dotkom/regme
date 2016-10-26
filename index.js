b5ee9019e752e3709cbf0102b23ac3083728e9aa
Jarle Trollebø <aborysa@gmail.com>
2016-10-26 19:05:34 +0200

    Made app component a 'complex' component

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+require('./styles/base.less')
 
 import Header from './components/header'
 import Registration from './components/registration'
+import Options from './components/options'
 
-const App = () => {
-  let title = "Bedrift"
-  return(
-    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <Header />
-      <Registration title={ title } />
-    </div>
-  )
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      options: null
+    }
+  }
+  set options(newOptions){
+    this.setState(Object.assign({},this.state,{options:newOptions}))
+  }
+  get options(){
+    return this.state.options
+  }
+  render(){
+    return(
+      <div className='mdl-layout mdl-js-layout mdl-layout--fixed-header'>
+        <Header />
+        <main className='mdl-layout__content '>
+          <div className='mdl-grid'>
+            <div className='mdl-cell mdl-cell--8-col-desktop mdl-cell--2-offset-desktop mdl-cell--6-col-tablet mdl-cell--1-offset-tablet mdl-cell--4-col-phone'>
+              <Registration event={this.options ? this.options.event : null} title={ 'Bedrift' } />
+              <Options onOptionsChanged={(options) => this.options = options}/>
+            </div>
+          </div>
+        </main>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
