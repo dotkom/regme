d248d2998940c1e78e29295b15f7732665cff330
Jarle Trollebø <aborysa@gmail.com>
2016-10-26 19:05:21 +0200

    Made options/index a 'complex' component

---
+++ components/options/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+import Events from './events'
+import Attendees from './attendees'
+
+
+class Options extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showOptions: false,
+      selectedEvent: null
+    }
+  }
+  clickHandler(){
+    this.setState(Object.assign({}, this.state, {showOptions: !this.state.showOptions}))
+  }
+  set event(event){
+    this.setState(Object.assign({},this.state,{selectedEvent: event}),() => {
+      if(this.props.onOptionsChanged){
+        this.props.onOptionsChanged({
+          event: event
+        })
+      }
+    })
+  }
+  get selectedEvent(){
+    return this.state.selectedEvent
+  }
+  render(){
+    let optionBody = ''
+    if(this.state.showOptions){
+      optionBody = (
+        <div>
+          <Events onEventChanged={ (event) => {this.event = event} } />
+          <Attendees event={ this.selectedEvent } />
+        </div>
+      )
+    }
+    return(
+      <div>
+        <a className='mdl-button mdl-js-button mdl-button--raised  mdl-js-ripple-effect mdl-button--accent' onClick={ () => {this.clickHandler()} }>Alternativer</a>
+        { optionBody }
+      </div>
+    )  
+  }
+
+}
+
+export default Options
+
