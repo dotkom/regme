ec63b2dc2915dacd4bf199282dcdd77accc5d9f2
Jarle Trollebø <aborysa@gmail.com>
2016-10-26 19:05:20 +0200

    Made attendees a 'complex' component

---
+++ components/options/attendees.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+import List from './list'
+
+class Attendees extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      waitlist: [],
+      attending: [],
+      notAttended: []
+    }
+    this.attendeesSub = null
+  }
+
+  componentWillReceiveProps(props){
+    if(this.attendeesSub){
+      this.attendeesSub.unsubscribe()
+      this.attendeesSub = null
+    }
+    if(props.event){
+
+      this.attendeesSub = props.event.attendees.subscribe((attendees)=>{
+        this.setState(Object.assign({},this.state,{
+          waitlist: attendees.waitlist.slice(0),
+          attending: attendees.attending.slice(0),
+          notAttended: attendees.notAttended.slice(0)
+        }))
+      })
+    }
+  }
+
+  componentWillUnmount(){
+    if(this.attendeesSub){
+      this.attendeesSub.unsubscribe()
+    }
+  }
+  
+  render(){
+    return (
+      <div>
+        <h3>Deltakere</h3>
+        <table className='mdl-data-table mdl-js-data-table attendee-lists'>
+          <List category='Møtt' attendees={this.state.attending} />
+          <List category='Ikke møtt' attendees={this.state.notAttended} />
+          <List category='Venteliste' attendees={this.state.waitlist} />
+        </table>
+      </div>
+    )
+  }
+}
+
+export default Attendees;
