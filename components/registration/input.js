f1edee92680da6ad77564d9e282895bc52042bfb
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-11-03 00:16:58 +0100

    Made Input focus on start only

--- components/registration/input.js
+++ components/registration/input.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 /**
  * The input field. Should only fetch input from
  * user or RFID-reader, then let the core
@@ -5,37 +6,51 @@
  * 
  * @prop {string} placeholder The placeholder text for the input field.
  */
-const Input = ( { placeholder, onSubmit, value } ) => {
-  let _input = null
-  let setRef = (ref) => {
-    _input = ref
-    if (ref) {
-      _input.focus()
-      _input.value = (value!=null) ? value : _input.value
-    }
+class Input extends Component {
+  constructor (props) {
+    super(props)
+    //this.placeholder = props.placeholder
+    //this.onSubmit = props.onSubmit
+    //this.value = props.value
+
+    let _input = null
   }
-  let submit = (evt) => {
+
+  componentDidMount () {
+    this._input.focus()
+  }
+
+  submit () {
     evt.preventDefault()
-    if(_input){
-      onSubmit(_input.value)
+    if(this._input) {
+      onSubmit(this._input.value)
     }
   }
 
-  return (
-    <form onSubmit={ submit }>
-      <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label registration-input">
-        <input className="mdl-textfield__input"
-          type="text"
-          id="registration-input"
-          pattern="[A-Za-z0-9]+"
-          autoComplete={false}
-          ref={setRef} />
-        <label className="mdl-textfield__label"
-          htmlFor="registration-input">{ placeholder }</label>
-        <span className="mdl-textfield__error">Input er ikke gyldig!</span>
-      </div>
-    </form>
-  )
+  setRef (ref) {
+    this._input = ref
+    if (ref) {
+      this._input.value = (this.value != null) ? this.value : this._input.value
+    }
+  }
+
+  render () {
+    return (
+      <form onSubmit={ this.submit.bind(this) }>
+        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label registration-input">
+          <input className="mdl-textfield__input"
+            type="text"
+            id="registration-input"
+            pattern="[A-Za-z0-9]+"
+            autoComplete={false}
+            ref={(a)=>this.setRef(a)} />
+          <label className="mdl-textfield__label"
+            htmlFor="registration-input">{ this.props.placeholder }</label>
+          <span className="mdl-textfield__error">Input er ikke gyldig!</span>
+        </div>
+      </form>
+    )
+  }
 }
 
 export default Input
