a486430638ecc2d51e8be372378647d8d8ea845d
Jarle Trollebø <aborysa@gmail.com>
2016-11-23 21:05:14 +0100

    Added toggle collapse to attendeelist

--- components/options/list.js
+++ components/options/list.js
@@ -1,31 +1,57 @@
+import React, { Component } from 'react'
 
-
-const List = ({ category, attendees, orderby, asc, onChange, collapse }) => {
-  
-  let userList = []
-  if(!collapse)
-    for(let attendee of attendees){
-      userList.push(
-        <tr key={attendee.id}>
-          <td className='mdl-data-table__cell--non-numeric'>{ [attendee.firstname, attendee.lastname].join(' ') }</td>
-          <td className='mdl-data-table__cell--non-numeric' colSpan="2">{ attendee.date.toLocaleString() }</td>
-        </tr>
-      )
+export default class List extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      collapse: false,
     }
-  let collapseContent = collapse ? (<i className="material-icons">keyboard_arrow_left</i>) : (<i className="material-icons">keyboard_arrow_down</i>)  
-  let tableHeaders = [
-    <th key="1" onClick={() => onChange("NAME",!asc)} className={'mdl-data-table__cell--non-numeric ' + (orderby=="NAME" ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending' ) : '')} colSpan='1'>{category}</th>,
-    <th key="2" onClick={() => onChange("DATE",!asc)} className={'mdl-data-table__cell--non-numeric ' + (orderby=="DATE" ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending' ) : '')} colSpan='1'>Registrerings dato</th>,
-    <th key="3" className='mdl-data-table_cell--non-numeric'>{collapseContent}</th>
-  ]
-  return (
-    <tbody>
-    <tr>
-      { tableHeaders }
-    </tr>
-    { userList }
-    </tbody>
-  )
+  }
+  toggleCollapse(){
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
+  render(){
+    let attendees = this.props.attendees
+    let category = this.props.category
+    let orderby = this.props.orderby
+    let asc = this.props.asc
+    let onChange = this.props.onChange
+    let collapse = this.state.collapse
+
+
+    let userList = []
+
+    if(!collapse)
+      for(let attendee of attendees){
+        userList.push(
+          <tr key={attendee.id}>
+            <td className='mdl-data-table__cell--non-numeric'>{ [attendee.firstname, attendee.lastname].join(' ') }</td>
+            <td className='mdl-data-table__cell--non-numeric' colSpan="2">{ attendee.date.toLocaleString() }</td>
+          </tr>
+        )
+      }
+    let collapseContent = collapse ? (<i className="material-icons">keyboard_arrow_left</i>) : (<i className="material-icons">keyboard_arrow_down</i>)  
+    let tableHeaders = [
+      <th key="1" onClick={() => onChange("NAME",!asc)} className={'mdl-data-table__cell--non-numeric ' + (orderby=="NAME" ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending' ) : '')} colSpan='1'>{category}</th>,
+      <th key="2" onClick={() => onChange("DATE",!asc)} className={'mdl-data-table__cell--non-numeric ' + (orderby=="DATE" ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending' ) : '')} colSpan='1'>Registrerings dato</th>,
+      <th key="3" onClick={() => this.toggleCollapse()} className='mdl-data-table_cell--non-numeric'>{collapseContent}</th>
+    ]
+    return (
+      <tbody>
+      <tr>
+        { tableHeaders }
+      </tr>
+      { userList }
+      </tbody>
+    )
+  }
 }
 
-export default List;
+/*
+const List = ({ category, attendees, orderby, asc, onChange }) => {
+
+}
+*/
+//export default List;
