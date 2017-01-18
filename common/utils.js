cf51624775134dec7e1269e5c13ea953e429a0c4
Sardred <andelar96@gmail.com>
2017-01-18 18:33:23 +0100

    Some eslint fixes for common and index

--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,7 @@
-export function isRfid(rfid){
-  return typeof(rfid) === "string" && /^\d{8,10}$/.test(rfid);
+export function isRfid(rfid) {
+  return typeof (rfid) === 'string' && /^\d{8,10}$/.test(rfid);
 }
-export function showToast(body){
-  var notification = document.querySelector('.mdl-js-snackbar');
+export function showToast(body) {
+  const notification = document.querySelector('.mdl-js-snackbar');
   notification.MaterialSnackbar.showSnackbar(body);
 }
