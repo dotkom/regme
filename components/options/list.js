f581eea6561b433d21e9954089d9e9e7466e5f82
sardred <andelar96@gmail.com>
2017-02-22 20:36:02 +0100

    Removed MDL from List component

--- components/options/list.js
+++ components/options/list.js
@@ -20,23 +20,21 @@ export default class List extends Component {
     const onChange = this.props.onChange;
     const collapse = this.state.collapse;
 
-
     const userList = [];
 
     if (!collapse) {
       for (const attendee of attendees) {
         userList.push(
           <tr key={attendee.id}>
-            <td className="mdl-data-table__cell--non-numeric">{ [attendee.firstname, attendee.lastname].join(' ') }</td>
-            <td className="mdl-data-table__cell--non-numeric" colSpan="2">{ attendee.date.toLocaleString() }</td>
+            <td className="cell-non-numeric">{ [attendee.firstname, attendee.lastname].join(' ') }</td>
+            <td className="cell-non-numeric" colSpan="2">{ attendee.date.toLocaleString() }</td>
           </tr>
         );
       }
     }
-    const collapseContent = collapse ? (<i className="material-icons">keyboard_arrow_left</i>) : (<i className="material-icons">keyboard_arrow_down</i>);
     const tableHeaders = [
-      <th key="1" onClick={() => onChange('NAME', !asc)} className={`mdl-data-table__cell--non-numeric ${orderby == 'NAME' ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending') : ''}`} colSpan="1">{category}</th>,
-      <th key="2" onClick={() => onChange('DATE', !asc)} className={`mdl-data-table__cell--non-numeric ${orderby == 'DATE' ? (asc ? 'mdl-data-table__header--sorted-ascending' : 'mdl-data-table__header--sorted-descending') : ''}`} colSpan="1">Registreringsdato</th>,
+      <th key="1" onClick={() => onChange('NAME', !asc)} className='cell-non-numeric' colSpan="1">{category}</th>,
+      <th key="2" onClick={() => onChange('DATE', !asc)} className='cell-non-numeric' colSpan="1">Registreringsdato</th>,
       <th key="3" onClick={() => this.toggleCollapse()}>{collapseContent}</th>,
     ];
     return (
