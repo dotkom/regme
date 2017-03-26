d6b8ae3a2eadfc2bd862e3251f8aa535b07eb67c
Jarle Trollebø <aborysa@gmail.com>
2017-03-26 17:04:53 +0200

    The pseudo selector ':empty' does not seem to update correctly in safari and will not update when you dynamically add elements after initial draw, fiksed by physically removing the element instead of using 'display: none' with ':empty'

--- components/header/index.js
+++ components/header/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BASE } from 'common/constants';
 
 const Header = ({event}) => {
-  const companyImg = event && event.organization ? (<img src={`${BASE}${event.organization.thumbnail}`} alt={event.organization.name} />) : null;
+  const companyImg = event && event.organization ? (
+    <div className="corner-container">
+      <div className="corner">
+        <img src={`${BASE}${event.organization.thumbnail}`} alt={event.organization.name} />
+      </div>
+    </div>
+  ) : null;
   return (
     <header>
       <img src="assets/images/logo_online.svg" alt="Online"/>
-      <div className="corner-container">
-        <div className="corner">
-          {companyImg}
-        </div>
-      </div>
+      {companyImg}
     </header>
   );
 };
