a321ee14db92462805d19cacd65afd998a105434
Aborysa <aborysa@gmail.com>
2017-10-11 21:49:01 +0200

    Removed a console.log

--- app/src/common/errorReporting.js
+++ app/src/common/errorReporting.js
@@ -3,7 +3,6 @@ import Raven from 'raven-js';
 const sentryUrl = process.env.RG_SENTRY_DSN;
 
 export function initErrorReporting(){
-	console.log(sentryUrl);
 	if(sentryUrl){
 		Raven.config(sentryUrl).install();
 	}
